102fdc44874073505add72dfccdae9f882b0ddf5
Dennis Rotar <dennisrotar@gmail.com>
2026-04-02 18:54:30 +0500

    Реализована доменная логика.

---
+++ Models/DTOs/CreateEventRequestDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagerAPI.Models.DTOs
+{
+	public class CreateEventRequestDto : IValidatableObject
+	{
+		[Required(ErrorMessage = "Название мероприятия обязательно!")]
+		public string Title { get; set; } = string.Empty;
+
+		public string? Description { get; set; }
+
+		[Required(ErrorMessage = "Дата начала обязательна!")]
+		public DateTime StartAt { get; set; }
+
+		[Required(ErrorMessage = "Дата окончания обязательна!")]
+		public DateTime EndAt { get; set; }
+
+		// Кастомная валидация дат
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (EndAt <= StartAt)
+			{
+				yield return new ValidationResult(
+					"Дата окончания должна быть позже даты начала!",
+					new[] { nameof(EndAt) });
+			}
+		}
+	}
+}
---
+++ Models/DTOs/UpdateEventRequestDto.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EventManagerAPI.Models.DTOs
+{
+	public class UpdateEventRequestDto : IValidatableObject
+	{
+		[Required(ErrorMessage = "Название мероприятия обязательно!")]
+		public string Title { get; set; } = string.Empty;
+
+		public string? Description { get; set; }
+
+		[Required(ErrorMessage = "Дата начала обязательна!")]
+		public DateTime StartAt { get; set; }
+
+		[Required(ErrorMessage = "Дата окончания обязательна!")]
+		public DateTime EndAt { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (EndAt <= StartAt)
+			{
+				yield return new ValidationResult(
+					"Дата окончания должна быть позже даты начала!",
+					new[] { nameof(EndAt) });
+			}
+		}
+	}
+}
